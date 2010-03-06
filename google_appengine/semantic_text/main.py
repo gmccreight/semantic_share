b9c21aef175df0d1e922b1159bdbc673608949c3
Gordon McCreight <gordon@mccreight.com>
2010-03-06 12:02:17 -0800

    Can store and retrieve pages now

--- google_appengine/semantic_text/main.py
+++ google_appengine/semantic_text/main.py
@@ -14,23 +14,77 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# To deploy:
+# Deploy by being in ../ and typing
+# ./appcfg.py update semantic_text
+
+import cgi
+import os
+import base64
+
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
+from google.appengine.ext import db
+
+class Page(db.Model):
+    name = db.StringProperty(multiline=False)
+    text_content = db.StringProperty(multiline=True)
+    js_content = db.StringProperty(multiline=True)
+    version = db.IntegerProperty()
+    date = db.DateTimeProperty(auto_now_add=True)
 
 class MainHandler(webapp.RequestHandler):
 
-  # To deploy:
-  # Deploy by being in ../ and typing
-  # ./appcfg.py update semantic_text
+    def get(self):
+        page_name = self.request.get('page_name')
+        
+        if page_name is None or page_name == "":
+            self.redirect('/?page_name=' + base64.b16encode(os.urandom(10)))
+            return
 
-  def get(self):
-    self.response.out.write('here is the default page, yo')
+        page = Page.get_or_insert(page_name, name=page_name, text_content="fill in", js_content="fill in", version=0)
+        page_text_content = cgi.escape(page.text_content)
+        page_js_content = cgi.escape(page.js_content)
+        page_version = cgi.escape(str(page.version))
+
+        self.response.out.write('<form action="/update" method="post">')
+        self.response.out.write('<input name="page_name" type="hidden" value="%s">' % page_name)
+        self.response.out.write('<textarea name="page_text_content">%s</textarea>' % page_text_content)
+        self.response.out.write('<textarea name="page_js_content">%s</textarea>' % page_js_content)
+        self.response.out.write('<input type="submit" name="submit" value="save">')
+        self.response.out.write('</form>')
+
+        if page.version > 0:
+            self.response.out.write('<br><a href="/?page_name=' + page.name + '_version_%s">back one version</a>' % str(page.version - 1))
+
+class PageHandler(webapp.RequestHandler):
+
+    def post(self):
+        page = Page.get_or_insert(self.request.get('page_name'))
+        page.text_content = self.request.get('page_text_content')
+        page.js_content = self.request.get('page_js_content')
+        
+        version = page.version
+        if version is None:
+            version = 0
+        page.version=version + 1
+
+        page.put()
+
+        # Save an older version of the page
+        version_page = Page(key_name=page.name + '_version_' + str(version), name=page.name, text_content=page.text_content, js_content=page.js_content, version=page.version - 1)
+        version_page.put()
+
+        self.redirect('/?page_name=' + page.name)
 
 def main():
-  application = webapp.WSGIApplication([('/', MainHandler)],
-                                       debug=True)
-  util.run_wsgi_app(application)
+    application = webapp.WSGIApplication(
+                                     [('/', MainHandler),
+                                      ('/update', PageHandler)],
+                                     debug=True)
+
+    util.run_wsgi_app(application)
 
 
 if __name__ == '__main__':
-  main()
+    main()
